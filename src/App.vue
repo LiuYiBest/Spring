0d0a62f4f765b6bb665c5c69ab827e08a90a3528
LiuyiBest <1070692044@qq.com>
2021-08-18 12:18:40 +0800

    初始化SpringUI组件库

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>我app的div</div>
+  <Frank/>
+</template>
+
+<script>
+import Frank from './components/Frank.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Frank
+  }
+}
+</script>
