327166fb86b3e63b13cf61e80f98bc97fb5371c8
LiuyiBest <1070692044@qq.com>
2021-08-19 12:43:22 +0800

    Dialog完成关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>
-    <p>dialog 的文档</p>
-  </div>
+  <div>实例</div>
+  <Button @click="toggle">toggle</Button>
+  <Dialog v-model:visible="x" :onClickOverlay="false" :ok="f1" :cancel="f2">
+  </Dialog>
 </template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import { ref } from 'vue'
+
+export default {
+  components: { Dialog, Button },
+  setup() {
+    const x = ref(false)
+    const toggle = () => {
+      x.value = !x.value
+    }
+    const f1 = () => {
+      return false
+    }
+    const f2 = () => {}
+    return { x, toggle, f1, f2 }
+  },
+}
+</script>
