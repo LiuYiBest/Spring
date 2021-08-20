816c0ab8b4cb5f5fb0ac39c88b41bf32305ab166
LiuyiBest <1070692044@qq.com>
2021-08-20 22:08:28 +0800

    更新

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import App from './App.vue'
 import './index.scss'
 import 'github-markdown-css'
 import { router } from './router'
+import './assets/svg.js'
 import Markdown from './components/Markdown.vue'
 
 const app = createApp(App)
