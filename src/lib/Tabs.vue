1d7a05d2377d3eeb87d41d67a30b5b023718a217
chunfen <1070692044@qq.com>
2021-09-30 21:52:55 +0800

    修复Tabs在vue3的问题

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,70 +1,60 @@
 <template>
   <div class="gulu-tabs">
     <div class="gulu-tabs-nav" ref="container">
-      <div
-        class="gulu-tabs-nav-item"
-        v-for="(t, index) in titles"
-        :ref="
-          (el) => {
-            if (el) navItems[index] = el
-          }
-        "
-        @click="select(t)"
-        :class="{ selected: t === selected }"
-        :key="index"
-      >
-        {{ t }}
-      </div>
+      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
       <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
-      <component
-        class="gulu-tabs-content-item"
-        :class="{ selected: c.props.title === selected }"
-        v-for="c in defaults"
-        :is="c"
-      />
+      <component :is="current" :key="current.props.title" />
     </div>
   </div>
 </template>
 
-  <script lang="ts">
+<script lang="ts">
 import Tab from './Tab.vue'
-import { computed, ref, onMounted, onUpdated } from 'vue'
+import {
+  computed,
+  ref,
+  watchEffect,
+  onMounted
+} from 'vue'
 export default {
   props: {
     selected: {
-      type: String,
-    },
+      type: String
+    }
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([])
-    const indicator = ref<HTMLDivElement>(null)
-    const container = ref<HTMLDivElement>(null)
-    const x = () => {
-      const divs = navItems.value
-      const result = divs.filter((div) => div.classList.contains('selected'))[0]
-      console.log(result)
-      const { width } = result.getBoundingClientRect()
-      indicator.value.style.width = width + 'px'
-      const { left: left1 } = container.value.getBoundingClientRect()
-      const { left: left2 } = result.getBoundingClientRect()
-      const left = left2 - left1
-      indicator.value.style.left = left + 'px'
-    }
-    onMounted(x)
-    onUpdated(x)
+    const selectedItem = ref < HTMLDivElement > (null)
+    const indicator = ref < HTMLDivElement > (null)
+    const container = ref < HTMLDivElement > (null)
+    onMounted(() => {
+      watchEffect(() => {
+        const {
+          width
+        } = selectedItem.value.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const {
+          left: left1
+        } = container.value.getBoundingClientRect()
+        const {
+          left: left2
+        } = selectedItem.value.getBoundingClientRect()
+        const left = left2 - left1
+        indicator.value.style.left = left + 'px'
+      }, {
+        flush: 'post'
+      })
+    })
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
-      //@ts-ignore
-      if (tag.type !== Tab) {
+      // @ts-ignore
+      if (tag.type.name !== Tab.name) {
         throw new Error('Tabs 子标签必须是 Tab')
       }
     })
     const current = computed(() => {
-      return defaults.filter((tag) => {
-        return tag.props.title === props.selected
-      })[0]
+      return defaults.find(tag => tag.props.title === props.selected)
     })
     const titles = defaults.map((tag) => {
       return tag.props.title
@@ -73,19 +63,19 @@ export default {
       context.emit('update:selected', title)
     }
     return {
+      current,
       defaults,
       titles,
-      current,
       select,
-      navItems,
+      selectedItem,
       indicator,
-      container,
+      container
     }
-  },
+  }
 }
 </script>
 
-  <style lang="scss">
+<style lang="scss">
 $blue: #40a9ff;
 $color: #333;
 $border-color: #d9d9d9;
@@ -118,12 +108,6 @@ $border-color: #d9d9d9;
   }
   &-content {
     padding: 8px 0;
-    &-item {
-      display: none;
-      &.selected {
-        display: block;
-      }
-    }
   }
 }
 </style>
