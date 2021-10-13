d17dd38e1b58242c15ecc4f818af33ea9d66ce60
chunfen <1070692044@qq.com>
2021-10-14 01:06:12 +0800

    Switch支持不同的size

---
+++ src/components/SwitchCom/Switch3.demo.vue
@@ -0,0 +1,30 @@
+<demo>
+  支持不同的Size
+  </demo>
+  <template>
+    <Switch v-model:value="big" size="big" />
+    <br />
+    <Switch v-model:value="normal" />
+    <br />
+    <Switch v-model:value="small" size="small" />
+</template>
+  
+  <script lang="ts">
+import Switch from '../../lib/Switch.vue'
+import { ref } from 'vue'
+export default {
+  components: {
+    Switch,
+  },
+  setup() {
+    const big = ref(false);
+    const normal = ref(false);
+    const small = ref(false);
+    return {
+      big,
+      normal,
+      small,
+    }
+  }
+}
+</script>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,20 +1,41 @@
 <template>
-  <button :disabled="disabled"  class="spring-switch"
-          @click="toggle" :class="{'spring-checked':value}">
+  <button   class="spring-switch"
+          @click="toggle" :class="classes"  :disabled="disabled">
     <span></span>
   </button>
-
+<!--  :class="{'spring-checked':value}"-->
 </template>
+
 <script lang="ts">
+import {computed}  from 'vue'
 export default {
   props: {
-    value: Boolean,
+    value: { type: Boolean, default: false,},
+    disabled: {type:Boolean,default:false},
+    size: {
+      type: String,
+      default: "normal",
+    },
   },
+
   setup(props, context) {
     const toggle = () => {
       context.emit("update:value", !props.value);
     };
-    return { toggle };
+    const classes = computed(() => {
+      const {
+        value,
+        size,
+        disabled
+      } = props;
+      return {
+        ["spring-switch-checked"]: value,
+        [`spring-switch-size-${size}`]: size,
+        [`spring-switch-disabled`]: disabled,
+      };
+    });
+
+    return { toggle ,classes };
   }
 };
 </script>
@@ -22,34 +43,72 @@ export default {
 $h: 22px;
 $h2: $h - 4px;
 .spring-switch {
-  height: $h; width: $h * 2;
+  height: $h;
+  width: $h * 2;
   border: none;
   background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
+  //transition: background 250ms;
+
+  &.spring-switch-disabled {
+    cursor: not-allowed;
+    background: #bfbfbf;
+    >span {
+      background: #ffffff;
+    }
+  }
+
   > span {
     position: absolute;
-    top: 2px; left: 2px;
-    height: $h2; width: $h2;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
     background: white;
     border-radius: $h2 / 2;
     transition: all 250ms;
   }
-  &[disabled] {
-    cursor: not-allowed;
+  &.spring-switch-checked {
+    background: #1890ff;
   }
-  &.spring-checked {
-    background: #13ce66;
-    > span { left: calc(100% - #{$h2} - 2px); }
+
+  &.spring-switch-checked>span {
+      //background: #17cc7a ;
+      left: calc(100% - #{$h2} - 3px);
   }
+
   &:focus { outline: none; }
   &:active {
-    > span { width: $h2 + 4px; }
+    > span { width: $h2 + 2px; }
   }
   &.spring-checked:active {
     > span {
       width: $h2 + 4px;
       margin-left: -4px;
+    }
+  }
+  &.spring-switch-size-big {
+    width: $h * 3.5;
+  }
+
+  &.spring-switch-size-small {
+    width: $h * 1.5;
+    height: $h * 0.75;
+
+    >span {
+      width: $h2 * 0.7;
+      height: $h2 * 0.7;
+    }
+
+    &.spring-switch-checked>span {
+      left: calc(100% - #{$h2 * 0.7} - 2px);
+    }
+
+    &:active {
+      >span {
+        width: $h2;
+      }
     }
   }
 }
