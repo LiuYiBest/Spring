254eec5081fcebfca5d1be3ccd037e8db034da72
chunfen <1070692044@qq.com>
2021-10-13 16:52:46 +0800

    新建InputDemo.vue页面

---
+++ src/components/Demo/InputDemo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Input输入框组件</h1>
+    <hr>
+    <p>通过鼠标或键盘输入字符内容。</p>
+    <p>根据不同的需求，一共有四种不同状态的输入框</p>
+  <section>
+    <ul>
+      <li>normal</li>
+      <li>disabled</li>
+      <li>readonly</li>
+      <li>error</li>
+    </ul>
+  </section>
+  <Demo :component="Input1Demo" />
+  </div>
+</template>
+
+<script lang='ts'>
+import Input1Demo from '../InputCom/Input1.demo.vue';
+import Demo from './Demo.vue'
+
+export default {
+  components:{
+    Input1Demo,
+    Demo
+  },
+  setup(){
+    return {Input1Demo}
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+  ul{
+    list-style: circle;
+    padding: 0 20px;
+  }
+</style>
