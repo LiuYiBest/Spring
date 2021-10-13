c9a22e9405eb216e3f1c330685efad05dc48681b
chunfen <1070692044@qq.com>
2021-10-13 16:52:33 +0800

    注册为全局组件并使用

---
+++ src/components/InputCom/Input1.demo.vue
@@ -0,0 +1,19 @@
+<demo>
+  4个不同类型的input输入框
+</demo>
+
+<template>
+    <Input />
+</template>
+
+<script lang='ts'>
+import Input from '../../lib/Input.vue'
+export default {
+  components: {
+    Input,
+  },
+  setup() {
+  }
+}
+
+</script>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,4 +3,5 @@ export { default as Button } from './Button.vue';
 export { default as Tabs } from './Tabs.vue';
 export { default as Tab } from './Tab.vue';
 export { default as Dialog } from './Dialog.vue';
+export { default as Input } from './Input.vue';
 export { openDialog } from './openDialog';
