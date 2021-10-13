8bd075a255f39199a420f57cc311a4cd1e3155c8
chunfen <1070692044@qq.com>
2021-10-13 23:38:06 +0800

    完成TagDemo.vue

---
+++ src/components/Demo/TagDemo.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Tag 示例</h1>
+  <Demo :component="Tag1Demo"></Demo>
+</template>
+
+<script lang="ts">
+
+import Demo from "./Demo.vue";
+import Tag1Demo from "../TagCom/Tag1.demo.vue";
+export default {
+  components: {
+    Tag1Demo,
+    Demo,
+  },
+  setup() {
+    return {
+      Tag1Demo
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.tag-demo {
+  .eminent-ui-tag + .eminent-ui-tag {
+    margin-left: 16px;
+  }
+}
+</style>
